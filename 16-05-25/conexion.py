c888bf41912c07c64ec56b8f05f51d6ad90d88f6
Daniel-Espinoza <135719429+Luis-Daniel-Espinoza-Gonzales@users.noreply.github.com>
2025-06-13 19:26:54 -0300

    subo ejercicio 16-05-25 incompleto

--- 16-05-25/conexion.py
+++ 16-05-25/conexion.py
@@ -1,8 +1,28 @@
 import mysql.connector
 
-conexion = None
-try:
-    conexion = mysql.connector.connect(user = 'root', password = '', host = 'localhost', database = 'chat_26')
-    print('se ha conectado a mysql')
-except Exception as e:
-    print(f'fallo en la conexion a mysql: {e}')
+def crear_conexion():
+    try:
+        conexion = mysql.connector.connect(user = "root", password = "", host = "localhost", database = "chat_26")
+        print("funciono la conexion")
+    except Exception as e:
+        print(f"fallo la conexion a mysql: {e}")
+    
+    return conexion 
+
+def mostrar_datos(conexion, nickname):
+	try:
+		cursor=conexion.cursor()
+		query_select= "SELECT * FROM usuarios"
+		cursor.execute(query_select)
+		datos = cursor.fetchall()
+		for row in datos:
+			print(f"id: {row[0]}, nickname: {row[1]}" )
+	except Exception as e:
+		print(f"fallo la busqueda de select: {e}")
+		
+
+nickname = "dann"
+
+con = crear_conexion()
+
+mostrar_datos(con, nickname)
