d3dba3ebf90690051948bc5b2b8e53974475e7cb
Luis-Daniel-Espinoza-Gonzales <danidex129@gmail.com>
2025-09-28 23:59:36 -0300

    botto submodulo login2

---
+++ 19-09-25/Login2/app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.login
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Elementos del Layout
+        val emailText = findViewById<EditText>(R.id.editTextTextEmailAddress2)
+        val passText = findViewById<EditText>(R.id.editTextTextPassword2)
+        val button = findViewById<Button>(R.id.button2)
+
+        //lista de usuarios
+        val users = mapOf(
+            "admin@gmail.com" to "1234",
+            "daniel@gmail.com" to "asdf",
+            "usuario@gmail.com" to "tirate_de_un_puente"
+        )
+
+        button.setOnClickListener {
+            val email = emailText.text.toString().trim()
+            val pass = passText.text.toString().trim()
+
+            if (users.containsKey(email) && users[email] == pass) {
+                Toast.makeText(this, "Bienvenido $email", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
